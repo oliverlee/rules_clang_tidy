c54780e23ff742fa2fdfed7c5631eb477eed550d
Oliver Lee <oliverzlee@gmail.com>
2024-08-21 23:00:49 -0700

    allow deps to include test targets
    Change-Id: I228e14a393bc1b3e95b82a07ac91425cf679591e

---
+++ test/check_cc_test-deps_test.bash
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+set -euxo pipefail
+
+source test/prelude.bash
+
+check_setup example/dep-cc_test
+
+# This sets a 15 second idle timeout
+# https://github.com/bazelbuild/bazel/issues/11062
+unset TEST_TMPDIR
+
+bazel \
+  --bazelrc="$test_bazelrc" \
+  build \
+  //pass/...
+
+bazel \
+  --bazelrc="$test_bazelrc" \
+  build \
+  //fail/... 2>&1 | tee "$log" || true
+
+grep "no such target '//:baz'" "$log"
