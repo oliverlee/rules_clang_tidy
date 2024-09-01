a078936bdd70bf389e3daab24741899142cf77d2
Oliver Lee <oliverzlee@gmail.com>
2024-09-01 10:08:34 -0500

    fix `sed` opts on macOS
    Change-Id: I433f834e9ec28f85d42584c5f6c16bd645bf0afd

--- apply-fixes.bash
+++ apply-fixes.bash
@@ -5,7 +5,13 @@ set -euo pipefail
 apply_bin="$1"
 output_path="$2"
 
+if [ "$(uname)" == "Darwin" ]; then
+  SED_INPLACE="-i''"
+else
+  SED_INPLACE="-i"
+fi
+
 find "$output_path" -type f -name "*.yaml" \
-  | xargs sed -i -e "s+%workspace%+$BUILD_WORKSPACE_DIRECTORY+g"
+  | xargs sed "$SED_INPLACE" -e "s+%workspace%+$BUILD_WORKSPACE_DIRECTORY+g"
 
 "$apply_bin" "$output_path"
